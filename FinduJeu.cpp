68b60fc9d9cc20cb159348362d94d546b32ac909
finfins <florian.delavernhe@gmail.com>
2014-11-26 13:09:35 +0100

    Phase FinduJeu ajouter

---
+++ FinduJeu.cpp
@@ -0,0 +1,34 @@
+#ifndef Placement
+#define Placement
+
+
+
+#include <iostream>
+
+class FinduJeu : Phase 
+{
+public:
+	void jouer ();
+	Placement (Game g) {super(g);}	
+	
+};
+
+
+void FinduJeu::jouer()
+{
+
+	if (game.getJ2().getNbPion()>2)
+	{
+	std::cout<<" JOUEUR 2 VICTORIEUX"<<std::endl; 
+	}
+	if (game.getJ1().getNbPion()>2)
+	{
+	std::cout<<" JOUEUR 1 VICTORIEUX"<<std::endl; 
+	}
+
+}
+
+
+
+
+#endif
